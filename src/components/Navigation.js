583e5d6b3609be3066ccc6b5a16617609f263e08
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-17 19:48:24 +0700

    add animation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link as ScrollLink } from 'react-scroll'
 import content from '../content'
+import useStartAnimation from '../hooks/useStartAnimation'
 
 const Navigation = () => {
+    const animate = useStartAnimation()
+
     return (
         <div style={{backgroundColor: '#091c29'}} className='fixed top-0 w-screen z-50'>
             <div className="w-10/12 mx-auto py-3 flex items-center justify-between text-white font-dosis">
-                <h1 className='text-3xl font-bold'>{content.nav.logo}{' '}
+                <h1 className={`${animate? '': '-translate-x-20 opacity-0'} transform transition duration-2500 text-3xl font-bold`} >{content.nav.logo}{' '}
                     <span className='w-3 h-3 bg-red-500 rounded-full inline-block'></span>
                 </h1>
-                <div>{content.nav.links.map((link, index) => (
+                <div className={`${animate? '': 'translate-x-20 opacity-0'} transform transition duration-2500`}>
+                {content.nav.links.map((link, index) => (
                     <ScrollLink to={link.to} smooth={true} key={index}>
-                        <span className='text-xl mr-4 px-2 cursor-pointer border-b-2 border-transparent hover:border-blue-400'>{link.text}</span>
+                        <span className='font-bold text-lg mr-4 px-2 cursor-pointer border-b-2 border-transparent hover:border-blue-400'>{link.text}</span>
                     </ScrollLink>
                 ))}
                 </div>
