69879f03c7d4a40dd414403bf782cb1487751163
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-19 17:47:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
+import Stack from "./components/Stack";
+import Work from "./components/Work";
 
 function App() {
-  return (
-    <h1 className="text-4xl font-bold">Hello World</h1>
-  );
+	return (
+		<div>
+			<Navigation />
+			<Header />
+			<About />
+			<Stack />
+			<Work />
+			<Project />
+			<Contact />
+		</div>
+	);
 }
 
 export default App;
