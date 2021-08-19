24930fbe6e05c818663599722d0333d31ae48552
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-19 14:15:46 +0700

    Create Project.js

---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Project = () => {
+    return (
+        <div id='myproject' className='min-h-screen flex justify-center items-center bg-soft font-dosis'>
+            <h1>My Project</h1>
+        </div>
+    )
+}
+
+export default Project
