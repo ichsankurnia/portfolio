e30f3a4c839449610c6aae08027217e787ae1900
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-28 19:57:52 +0700

    Create projectContent.js

---
+++ src/content/projectContent.js
@@ -0,0 +1,202 @@
+import content from "."
+
+const imgPath = `${process.env.PUBLIC_URL}/assets/project`
+
+const projectContent = [
+    {
+        title: 'PT Nutech Integrasi',
+        desc: content.work.desc,
+        projects: [
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            },
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            },
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            },
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Freelance',
+        desc: content.work.desc,
+        projects: [
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            },
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            },
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Internship',
+        desc: content.work.desc,
+        projects: [
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Other Project',
+        desc: content.work.desc,
+        projects: [
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            },
+            {
+                img_thumb: imgPath + '/pwa_wheater_app.png',
+                title: 'PWA Wheater App',
+                short_desc: 'Progressive Web App Wheater Application using ReactJS and Service Worker',
+                long_desc: '',
+                tools_tech: ['ReactJS', 'PWA', 'Service Worker'],
+                list_img: [
+                    {
+                        img: imgPath + '/pwa_wheater_app.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    },
+                    {
+                        img: imgPath + '/pwa_wheater_app2.png',
+                        img_desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+export default projectContent
