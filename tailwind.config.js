ee327aeb34846d0fb82c3d9a5526e0cfdc994918
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-28 19:58:27 +0700

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,6 +64,7 @@ module.exports = {
       64: '16rem',
       72: '18rem',
       80: '20rem',
+      88: '22rem',
       96: '24rem',
     },
     animation: {
@@ -73,6 +74,8 @@ module.exports = {
       pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       pulse1s: 'pulse1s 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       bounce: 'bounce 1s infinite',
+      newbounce: 'newbounce 1s infinite',
+      rebounce: 'rebounce 1s infinite',
       float: 'float 3s infinite',
       refloat: 'refloat 3s infinite'
     },
@@ -157,6 +160,7 @@ module.exports = {
     borderWidth: {
       DEFAULT: '1px',
       0: '0px',
+      1: '1px',
       2: '2px',
       4: '4px',
       8: '8px',
@@ -485,6 +489,26 @@ module.exports = {
         },
         '50%': {
           transform: 'none',
+          animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+        },
+      },
+      newbounce: {
+        '0%, 100%': {
+          transform: 'translateY(-10%)',
+          animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+        },
+        '50%': {
+          transform: 'none',
+          animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+        },
+      },
+      rebounce: {
+        '0%, 100%': {
+          transform: 'none',
+          animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+        },
+        '50%': {
+          transform: 'translateY(-10%)',
           animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
         },
       },
